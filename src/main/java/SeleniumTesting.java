c1221a9eb772377158608ce2a894bee240ae765b
gasparhe <gasparhe@gmail.com>
2024-04-24 13:13:40 +0200

    ejemplos de testing

--- src/main/java/SeleniumTesting.java
+++ src/main/java/SeleniumTesting.java
@@ -1,18 +1,30 @@
+
+import java.io.File;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class SeleniumTesting {
-	public static void main(String[] args) {
-		// Configura la propiedad del sistema para el driver de Chrome
-		System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
 
-		// Crea una instancia de ChromeDriver
-		WebDriver driver = new FirefoxDriver();
+    public static void main(String[] args) {
+        // Configura la propiedad del sistema para el driver de Chrome
+        System.setProperty("webdriver.gecko.driver", "resources/geckodriver.exe");
 
-		// Abre una página web
-		driver.get("http://www.google.com");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
+        WebDriver driver = new FirefoxDriver(options);
 
-		// Cierra el navegador
-		driver.quit();
-	}
+        driver.get("https://bugbug.io/blog/testing-frameworks/best-selenium-practice-websites/");
+        // 2 | setWindowSize | 1411x978 | 
+        driver.manage().window().setSize(new Dimension(1411, 978));
+        // 3 | click | linkText=Automation Testing Website | 
+        driver.findElement(By.linkText("Automation Testing Website")).click();
+        // 4 | click | linkText=Selenium Cheat Sheet | 
+        driver.findElement(By.linkText("Selenium Cheat Sheet")).click();
+        // Cierra el navegador
+        driver.quit();
+    }
 }
