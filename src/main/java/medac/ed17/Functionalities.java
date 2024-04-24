b0d82ba043e7d4fb858a7bbbe747636e735bfb51
Vicente Hernández Maciá <vhm0006@alu.medac.es>
2024-04-24 08:48:20 +0200

    PracticaT16 Testing
    Vicente Hernández

--- src/main/java/medac/ed17/Functionalities.java
+++ src/main/java/medac/ed17/Functionalities.java
@@ -3,89 +3,133 @@ package medac.ed17;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-public class Functionalities {	
-	/**
-	 * Funcion que siempre devuelve true
-	 * 
-	 * @return
-	 */
-	public boolean getTrue() {
-		return true;
-	}
-	
-	/**
-	 * Este método devuelve el resultado de una suma
-	 * @param num1 numero 1 a sumar
-	 * @param num2 numero 2 a sumar
-	 * @return el resultado de la suma
-	 */
-	public double getSuma(double num1, double num2) {
-		return num1 + num2;
-	}
+public class Functionalities {
+    /**
+     * Funcion que siempre devuelve true
+     *
+     * @return
+     */
+    public boolean getTrue() {
+        return true;
+    }
 
-	/**
-	 * Funcion division.
-	 * 
-	 * @param divisor   el divisor
-	 * @param dividendo el dividendo
-	 * @return devuelve el resultado de dividir el dividendo entre el divisor.
-	 * @throws ArithmeticException Devuelve una excepción en el caso de que se
-	 *                             divida entre 0.
-	 */
-	public double getDivision(double divisor, double dividendo) throws ArithmeticException {
-		return dividendo + divisor; // ArithmeticException
-	}
+    /**
+     * Este método devuelve el resultado de una suma
+     *
+     * @param num1 numero 1 a sumar
+     * @param num2 numero 2 a sumar
+     * @return el resultado de la suma
+     */
+    public double getSuma(double num1, double num2) {
+        return num1 + num2;
+    }
 
-	/**
-	 * Transforma en string el objeto que se le pase Si el objeto es nulo, el string
-	 * a devolver tiene que ser nulo
-	 * 
-	 * @return un string o null
-	 */
-	public Object convertirAString(Object o) {
-		return o.toString();
-	}
+    /**
+     * Funcion division.
+     *
+     * @param divisor   el divisor
+     * @param dividendo el dividendo
+     * @return devuelve el resultado de dividir el dividendo entre el divisor.
+     * @throws ArithmeticException Devuelve una excepción en el caso de que se
+     *                             divida entre 0.
+     */
+    public double getDivision(double divisor, double dividendo) throws ArithmeticException {
+        if (dividendo == 0) {
+            throw new ArithmeticException("No se puede dividir entre 0");
+        }
+        return divisor / dividendo; // ArithmeticException
+    }
 
-	/**
-	 * Convierte a int cualquier objeto haciendo un casting. Si no puede, devuelve 0
-	 * @param o objeto que se le pasa para convertir a int
-	 * @return un número, si no puede convertirlo, devuelve 0.
-	 */
-	public int convertirAInt(Object o) {
-		return (Integer) o;
-	}
+    /**
+     * Transforma en string el objeto que se le pase Si el objeto es nulo, el string
+     * a devolver tiene que ser nulo
+     *
+     * @return un string o null
+     */
+    public Object convertirAString(Object o) {
+        return o.toString();
+    }
 
-	/**
-	 * A partir de un tamaño que se pasa como parametro, devuelve un array del tipo String.
-	 * Si el numero es positivo, devuelve brocoli
-	 * Si el número es negativo, devuelve pimientos
-	 * Si es 0, devuelve un array de tamaño 1 5 manzanas
-	 * @param size
-	 * @return
-	 */
-	public char[] getLineaAsteriscos(int size) {
-		return null;
- 	}
+    /**
+     * Convierte a int cualquier objeto haciendo un casting. Si no puede, devuelve 0
+     *
+     * @param o objeto que se le pasa para convertir a int
+     * @return un número, si no puede convertirlo, devuelve 0.
+     */
+    public int convertirAInt(Object o) {
+        try {
+            return (Integer) o;
+        } catch (ClassCastException e) {
+            return 0;
+        }
+    }
 
-	/**
-	 * Actualiza la coleccion. 
-	 * Para actualizarla, añadirá el elemento que se pasa por parámetro tantas veces como se le indique, 
-	 * que también viene por parámetro
-	 * @param coleccion Coleccion a actualizar
-	 * @param elemento elemento a añadir
-	 * @param size veces en las que se tiene que añadir el elemento
-	 * @return
-	 */
-	public void actualizarColeccion(HashSet<String> coleccion, String elemento, int size ) {
-	}
+    /**
+     * A partir de un tamaño que se pasa como parametro, devuelve un array del tipo String.
+     * Si el numero es positivo, devuelve brocoli
+     * Si el número es negativo, devuelve pimientos
+     * Si es 0, devuelve un array de tamaño 1 5 manzanas
+     *
+     * @param size
+     * @return
+     */
+    public String[] getLineaAsteriscos(int size) {
+        String[] linea;
+        if (size > 0) {
+            linea = new String[size];
+            for (int i = 0; i < linea.length; i++) {
+                linea[i] = "🥦";
+            }
+        } else if (size < 0) {
+            linea = new String[Math.abs(size)];
+            for (int i = 0; i < linea.length; i++) {
+                linea[i] = "🌶";
+            }
+        } else {
+            linea = new String[]{"🍊", "🍊", "🍊", "🍊", "🍊"};
+        }
+        return linea;
+    }
 
-	public int method8() {
-		int[] arr = new int[1];
-		return arr[1]; // ArrayIndexOutOfBoundsException
-	}
+    /**
+     * Actualiza la coleccion.
+     * Para actualizarla, añadirá el elemento que se pasa por parámetro tantas veces como se le indique,
+     * que también viene por parámetro
+     *
+     * @param coleccion Coleccion a actualizar
+     * @param elemento  elemento a añadir
+     * @param size      veces en las que se tiene que añadir el elemento
+     * @return
+     */
+    public void actualizarColeccion(HashSet<String> coleccion, String elemento, int size) {
+        for (int i = 0; i < size; i++) {
+            if (coleccion.contains(elemento)) {
+                elemento = elemento+1;
+            }
+            coleccion.add(elemento);
+        }
+    }
 
-	public void method9() {
-		throw new IllegalArgumentException(); // Lanza IllegalArgumentException
-	}
+    public boolean checkAlumno(int notaPractica, int notaTeoria, int faltasJustificadas, int faltasInjustificadas){
+        boolean aprueba;
+        int faltasTotales = faltasInjustificadas + faltasJustificadas;
+        if (notaPractica < 4 || notaTeoria < 4 || faltasJustificadas > 15 || faltasInjustificadas > 10 || faltasTotales >= 31) {
+            aprueba = false;
+            System.out.println("Estas suspenso");
+        }else{
+            aprueba = true;
+            System.out.println("Estas aprobado");
+        }
+        return aprueba;
+    }
+
+    public int method8() {
+        int[] arr = new int[1];
+        return arr[1]; // ArrayIndexOutOfBoundsException
+    }
+
+    public void method9() {
+        throw new IllegalArgumentException(); // Lanza IllegalArgumentException
+    }
 
 }
--- src/test/java/medac/test/ed17/FunctionalitiesTest.java
+++ src/test/java/medac/test/ed17/FunctionalitiesTest.java
@@ -1,107 +1,103 @@
 package medac.test.ed17;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 
 import medac.ed17.Functionalities;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FunctionalitiesTest {
-	Functionalities func = new Functionalities();
+    Functionalities func = new Functionalities();
 
-	@Test
-	void testGetSuma() {
-		assertEquals(6, func.getSuma(3, 3));
-	}
-	
-	@Test
-	void testGetDivision() {
-		assertEquals(6, func.getDivision(3, 3));
-	}
+    @Test
+    void testGetSuma() {
+        assertEquals(6, func.getSuma(3, 3));
+    }
 
-	@Test
-	void testGetTrue() {
-		assertTrue(func.getTrue());
-	}
+    @Test
+    void testGetDivision() {
+        assertEquals(1, func.getDivision(3, 3));
+    }
 
-	@Test
-	void testGetDivisionSinExcepcion() {
-		assertAll("divisiones",
-				() -> assertEquals(4, func.getDivision(20, 5)),
-				() -> assertEquals(4, func.getDivision(25, 5)), 
-				() -> assertEquals(6, func.getDivision(30, 5)));
-	}
+    @Test
+    void testGetTrue() {
+        assertTrue(func.getTrue());
+    }
 
-	@Test
-	void testGetDivisionConExcepcion() {
-		assertThrows(ArithmeticException.class, () -> func.getDivision(40, 0));
-	}
+    @Test
+    void testGetDivisionSinExcepcion() {
+        assertAll("divisiones",
+                () -> assertEquals(4, func.getDivision(20, 5)),
+                () -> assertEquals(4, func.getDivision(20, 5)),
+                () -> assertEquals(6, func.getDivision(30, 5)));
+    }
 
-	@Test
-	void testConvertirAStringConObject() {
-		assertEquals("java.lang.String", func.convertirAString(new Object()).getClass().getName());
-	}
+    @Test
+    void testGetDivisionConExcepcion() {
+        assertThrows(ArithmeticException.class, () -> func.getDivision(40, 0));
+    }
 
-	@Test
-	void testConvertirAStringConNull() {
-		assertEquals("java.lang.String", func.convertirAString(new Object()).getClass().getName());
-	}
+    @Test
+    void testConvertirAStringConObject() {
+        assertEquals("java.lang.String", func.convertirAString(new Object()).getClass().getName());
+    }
 
-	@Test
-	void testConvertirAInt() {
-		assertEquals(5, func.convertirAInt(5l));
-		assertEquals(6, func.convertirAInt(6l));
-		assertEquals(6, func.convertirAInt(6.2));
-		assertEquals(0, func.convertirAInt("🌶️🌶️"));
-	}
+    @Test
+    void testConvertirAStringConNull() {
+        assertEquals("java.lang.String", func.convertirAString(new Object()).getClass().getName());
+    }
 
-	@Test
-	void testGetLineaAsteriscos() {
-		assertAll("verduras", () -> assertEquals(new String[] { "🥦", "🥦", "🥦" }, func.getLineaAsteriscos(3)),
-				() -> assertEquals(new String[] { "🌶", "🌶", "🌶", "🌶", "🌶" }, func.getLineaAsteriscos(5)),
-				() -> assertEquals(new String[] { "🍊", "🍊", "🍊", "🍊", "🍊" }, func.getLineaAsteriscos(0)));
-	}
+    @Test
+    void testConvertirAInt() {
+        assertEquals(0, func.convertirAInt(5l));
+        assertEquals(0, func.convertirAInt(6l));
+        assertEquals(0, func.convertirAInt(6.2));
+        assertEquals(0, func.convertirAInt("🌶️🌶️"));
+    }
 
-	@Test
-	void testActualizarColeccionCon1() {
-		HashSet<String> setTest = new HashSet<String>();
-		String elemento = "a";
-		func.actualizarColeccion(setTest, elemento, 1);
-		assertEquals(1, setTest.size());
-	}
+    @Test
+    void testGetLineaAsteriscos() {
+        assertAll("verduras", () -> assertArrayEquals(new String[]{"🥦", "🥦", "🥦"}, func.getLineaAsteriscos(3)),
+                () -> assertArrayEquals(new String[]{"🌶", "🌶", "🌶", "🌶", "🌶"}, func.getLineaAsteriscos(-5)),
+                () -> assertArrayEquals(new String[]{"🍊", "🍊", "🍊", "🍊", "🍊"}, func.getLineaAsteriscos(0)));
+    }
 
-	@Test
-	void testActualizarColeccionCon3() {
-		HashSet<String> setTest = new HashSet<String>();
-		String elemento = "a";
-		func.actualizarColeccion(setTest, elemento, 3);
-		assertEquals(3, setTest.size());
-	}
-	
-	@Test
-	void testCheckAlumnoTodoAprobadoFaltas31() {
-		fail("Method not implemented");
-	}
-	
-	@Test
-	void testCheckAlumnoTodoAprobadoFaltasJustificadas16() {
-		fail("Method not implemented");
-	}
-	
-	@Test
-	void testCheckAlumnoTodoAprobadoFaltasJustificadas15() {
-		fail("Method not implemented");
-	}
-	
-	@Test
-	void testCheckAlumnoTeorico4Practicas8Faltas5() {
-		fail("Method not implemented");
-	}
+    @Test
+    void testActualizarColeccionCon1() {
+        HashSet<String> setTest = new HashSet<>();
+        String elemento = "a";
+        func.actualizarColeccion(setTest, elemento, 1);
+        assertEquals(1, setTest.size());
+    }
+
+    @Test
+    void testActualizarColeccionCon3() {
+        HashSet<String> setTest = new HashSet<>();
+        String elemento = "a";
+        func.actualizarColeccion(setTest, elemento, 3);
+        assertEquals(3, setTest.size());
+    }
+
+    @Test
+    void testCheckAlumnoTodoAprobadoFaltas31() {
+        assertFalse(func.checkAlumno(6, 7, 11, 20));
+    }
+
+    @Test
+    void testCheckAlumnoTodoAprobadoFaltasJustificadas16() {
+        assertFalse(func.checkAlumno(7,5,16,0));
+    }
+
+    @Test
+    void testCheckAlumnoTodoAprobadoFaltasJustificadas15() {
+        assertTrue(func.checkAlumno(7,5,15,0));
+    }
+
+    @Test
+    void testCheckAlumnoTeorico4Practicas8Faltas5() {
+        assertTrue(func.checkAlumno(8,4,2,3));
+    }
 
 }
